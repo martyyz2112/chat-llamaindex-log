d276e4694d4a27bcdb876c5c2853724558750b20
martyyz2112 <53046977+martyyz2112@users.noreply.github.com>
2024-02-13 09:26:20 -0300

    Add files via upload

--- app/bots/bot.data.ts
+++ app/bots/bot.data.ts
@@ -12,43 +12,13 @@ type DemoBot = Omit<Bot, "session">;
 export const DEMO_BOTS: DemoBot[] = [
   {
     id: "1",
-    avatar: "1f916",
-    name: "GPT-4 Vision Preview",
+    avatar: "1f69d",
+    name: "Wazuh",
     botHello: "Hello! How can I assist you today?",
-    context: [],
-    modelConfig: {
-      model: "gpt-4-vision-preview",
-      temperature: 0.3,
-      maxTokens: 4096,
-      sendMemory: false,
-    },
-    readOnly: true,
-    hideContext: false,
-  },
-  {
-    id: "2",
-    avatar: "1f916",
-    name: "My Documents",
-    botHello: "Hello! How can I assist you today?",
-    context: [],
-    modelConfig: {
-      model: "gpt-4-1106-preview",
-      temperature: 0.5,
-      maxTokens: 4096,
-      sendMemory: true,
-    },
-    readOnly: true,
-    hideContext: false,
-  },
-  {
-    id: "3",
-    avatar: "1f5a5-fe0f",
-    name: "Red Hat Linux Expert",
-    botHello: "Hello! How can I help you with Red Hat Linux?",
     context: [
       {
         role: "system",
-        content: TEMPLATE("Red Hat Linux Expert"),
+        content: TEMPLATE("You're a useful Data Security Analyst that can provide in depth, detailed analysis, summarizes in 200 words, and answer all the questions"),
       },
     ],
     modelConfig: {
@@ -57,50 +27,8 @@ export const DEMO_BOTS: DemoBot[] = [
       maxTokens: 4096,
       sendMemory: true,
     },
-    readOnly: true,
-    datasource: "redhat",
-    hideContext: false,
-  },
-  {
-    id: "4",
-    avatar: "1f454",
-    name: "Apple Watch Genius",
-    botHello: "Hello! How can I help you with Apple Watches?",
-    context: [
-      {
-        role: "system",
-        content: TEMPLATE("Apple Genius specialized in Apple Watches"),
-      },
-    ],
-    modelConfig: {
-      model: "gpt-4-1106-preview",
-      temperature: 0.1,
-      maxTokens: 4096,
-      sendMemory: true,
-    },
-    readOnly: true,
-    datasource: "watchos",
-    hideContext: false,
-  },
-  {
-    id: "5",
-    avatar: "1f4da",
-    name: "German Basic Law Expert",
-    botHello: "Hello! How can I assist you today?",
-    context: [
-      {
-        role: "system",
-        content: TEMPLATE("Lawyer specialized in the basic law of Germany"),
-      },
-    ],
-    modelConfig: {
-      model: "gpt-4-1106-preview",
-      temperature: 0.1,
-      maxTokens: 4096,
-      sendMemory: true,
-    },
-    readOnly: true,
-    datasource: "basic_law_germany",
+    readOnly: false,
+    datasource: "wazuh",
     hideContext: false,
   },
 ];
